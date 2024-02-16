6e3a5379f2192302839b9e4c39dfcdee66a6b5cd
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-02-16 12:17:41 -0300

    test: initial jest tests

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // render the component
+    const { container } = render(<Main />)
+
+    // search the element and verify if it exists
+    expect(
+      screen.getByRole('heading', { name: /hello world/i })
+    ).toBeInTheDocument()
+
+    // snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
